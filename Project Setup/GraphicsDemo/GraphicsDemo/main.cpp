a7ff920cf2d5adf5096d8f0e39b8401dee25cb7d
JoyLee Flahive <joylee.flahive@gmail.com>
2015-01-13 23:05:58 -0500

    Adding OpenGL Libraries needed for glew and started setting up GraphicsDemo project to use it.

---
+++ Project Setup/GraphicsDemo/GraphicsDemo/main.cpp
@@ -0,0 +1,17 @@
+/*
+Project: GraphicsDemo
+Author: JoyLee Flahive
+File: main.cpp
+Description:
+This project should serve as a base for further
+projects in Game Physics.
+*/
+
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+	
+
+	return 0;
+}
