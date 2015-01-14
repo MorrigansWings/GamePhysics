724e38895039c08f188edb3c87c77b5844d2b631
JoyLee Flahive <joylee.flahive@gmail.com>
2015-01-14 01:50:33 -0500

    GraphicsDemo now draws a basic window successfully with no link errors - freeglut and glew libraries seem to be functional. Added folder "OpenGL Libraries" to hold all needed library files.

--- Project Setup/GraphicsDemo/GraphicsDemo/main.cpp
+++ Project Setup/GraphicsDemo/GraphicsDemo/main.cpp
@@ -7,11 +7,19 @@ This project should serve as a base for further
 projects in Game Physics.
 */
 
+#include "Dependencies\glew\glew.h"
+#include "Dependencies\freeglut\freeglut.h"
 #include <iostream>
 
 int main(int argc, char ** argv)
 {
-	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(500, 500);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("OpenGL Test Window");
+
+	glutMainLoop();
 
 	return 0;
 }
